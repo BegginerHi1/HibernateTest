ba21ef3145661a716bc5f4fb3c3469ff621e51db
BegginerHi1 <146478622+BegginerHi1@users.noreply.github.com>
2023-12-05 12:55:27 +0300

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     private UserDao userDao = new UserDaoHibernateImpl();
+    
     public void createUsersTable() {
         userDao.createUsersTable();
     }
